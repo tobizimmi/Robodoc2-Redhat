cd9efbac020b33cae63f57d66af689da96ce6ea6
tobizimmi <36736617+tobizimmi@users.noreply.github.com>
2026-08-24 22:48:31 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@ ini_set('display_errors','1');
 error_reporting(E_ALL);
 
 chdir(__DIR__);
-require_once 'bootstrap.php';
+require_once __DIR__ . '/../app/bootstrap.php';
 
 $router = new Router();
 
